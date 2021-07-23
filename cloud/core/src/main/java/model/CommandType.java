13f111acb6384e53b405c1241887ae11e1836c0f
Evgeny Khavdey <khavdey61@yandex.ru>
2021-07-23 15:32:26 +0300

    облако с авторизацией

---
+++ cloud/core/src/main/java/model/CommandType.java
@@ -0,0 +1,11 @@
+package model;
+
+public enum CommandType {
+    AUTHORIZATION,
+    AUTHORIZATION_PASSED,
+    FILE_DELETE,
+    FILE_MESSAGE,
+    FILE_REQUEST,
+    LIST_REQUEST,
+    LIST_RESPONSE
+}
